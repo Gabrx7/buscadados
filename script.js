f4c5915e7a76187f447c968eb513aeb762ac7edd
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2022-12-07 16:03:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function LinkReferencia(){
+    window.location.href = 'https://chat.whatsapp.com/G1yHeKPD5sF58MIdOjONDs'
+}
+
+function LinkGabriel(){
+    window.location.href = 'https://api.whatsapp.com/send?phone=5544997619331&text=Opa%20Gabriel!%20gostaria%20de%20puxar%20dados%20de%20uma%20pessoa'
+}
+
+function LinkServidores(){
+    window.location.href = 'https://api.whatsapp.com/send?phone=5544997619331&text=Ol%C3%A1,%20gostaria%20de%20comprar%20os%20servidores%20para%20busca%20de%20dados'
+}
